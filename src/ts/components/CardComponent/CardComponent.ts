916b9698ca89f1765ab2a43538dca7f87d12ff65
Marc Girbau <marc.girbau94@gmail.com>
2023-01-24 22:24:00 +0100

    Create OverlayCardComponent class and instance it

--- src/ts/components/CardComponent/CardComponent.ts
+++ src/ts/components/CardComponent/CardComponent.ts
@@ -2,6 +2,7 @@ import { type Character } from "../../Character/Character";
 import { CardBodyComponent } from "../CardBodyComponent/CardBodyComponent";
 import { Component } from "../Component/Component";
 import { ImageComponent } from "../ImageComponent/ImageComponent";
+import { OverlayCardComponent } from "../OverlayCardComponent/OverlayCardComponent";
 
 interface CardComponentStructure {
   element: Element;
@@ -25,5 +26,7 @@ export class CardComponent extends Component implements CardComponentStructure {
     );
 
     const body = new CardBodyComponent(this.element, character);
+
+    const overlay = new OverlayCardComponent(this.element, character);
   }
 }
